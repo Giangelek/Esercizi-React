b579523751684a935c39c06d0881bef131080a90
Gianfranco D'Angelo <Gianfranco.dangelo94@gmail.com>
2022-11-07 15:40:17 +0100

    es10 eseguito

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { HelloWorld } from "./hello";
+import { Welcome } from "./welcome";
 
 export class App extends React.Component{
     render(){
         return(
             <div>
                 <HelloWorld />
+                <Welcome name="Enzuccio" />
             </div>
         )
 
