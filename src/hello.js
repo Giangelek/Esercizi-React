1580085ed0bdc37f55136d9733465012d5923558
Gianfranco D'Angelo <Gianfranco.dangelo94@gmail.com>
2022-11-07 15:26:11 +0100

    es8 eseguito

--- src/hello.js
+++ src/hello.js
@@ -2,11 +2,11 @@ import React from "react";
 
 export class HelloWorld extends React.Component {
     render(){
-        return (
-            <div>
-                <h1>Hello, World!</h1>
-                <p>What a beautiful day!</p>
-            </div>
-        )
+       return(
+        <div>
+            <h1>"Hello, World!"</h1>
+            <p>"What a beautiful day!"</p>
+        </div>
+       )
     }
 }
