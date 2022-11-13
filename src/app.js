aff90f3c300ef426d9b0aea5c7f657fe0b26f48d
Gianfranco D'Angelo <Gianfranco.dangelo94@gmail.com>
2022-11-13 16:23:51 +0100

    cond rend 04 eseguito

--- src/app.js
+++ src/app.js
@@ -3,14 +3,12 @@ import { HelloWorld } from "./Hello";
 import { Message } from "./Message";
 import { Welcome } from "./Welcome";
 
-const strongName= <strong>Enzuccio Zappaterra</strong>
-
 export class App extends React.Component{
     render(){
         return(
             <div>
                 <HelloWorld />
-                <Welcome name={strongName} age={64} />
+                <Welcome name="John" age={64} />
             </div>
         )
 
