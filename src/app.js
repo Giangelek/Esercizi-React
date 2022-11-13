acf5e052633482c6ebdd2e0c47f46aa4441a6338
Gianfranco D'Angelo <Gianfranco.dangelo94@gmail.com>
2022-11-13 15:46:01 +0100

    esercizio prop 04 eseguito

--- src/app.js
+++ src/app.js
@@ -3,12 +3,14 @@ import { HelloWorld } from "./hello";
 import { Message } from "./message";
 import { Welcome } from "./welcome";
 
+const strongName= <strong>Enzuccio Zappaterra</strong>
+
 export class App extends React.Component{
     render(){
         return(
             <div>
                 <HelloWorld />
-                <Welcome name="Enzuccio Zappaterra" age="87" />
+                <Welcome name={strongName} age={87} />
             </div>
         )
 
