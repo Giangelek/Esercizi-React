0caf1dcdd9a2445f809252110d5ae587300309d7
Gianfranco D'Angelo <Gianfranco.dangelo94@gmail.com>
2022-11-13 15:38:44 +0100

    esercizio 03 Prop eseguito

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ export class App extends React.Component{
         return(
             <div>
                 <HelloWorld />
-                <Welcome />
+                <Welcome name="Enzuccio Zappaterra" age="87" />
             </div>
         )
 
