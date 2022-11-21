384e0337d225b13c06d9fccf2156943567c26447
Gianfranco D'Angelo <Gianfranco.dangelo94@gmail.com>
2022-11-21 12:13:23 +0100

    state01 eseguito

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Counter } from "./Counter";
 import { HelloWorld } from "./Hello";
 import { Message } from "./Message";
 import { Welcome } from "./Welcome";
@@ -9,6 +10,7 @@ export class App extends React.Component{
             <div>
                 <HelloWorld />
                 <Welcome name="John" age={12} />
+                <Counter />
             </div>
         )
 
