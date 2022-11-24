12dd4a427e4f792b49dec2ecf4b2e121003d61e2
Gianfranco D'Angelo <Gianfranco.dangelo94@gmail.com>
2022-11-24 13:19:27 +0100

    list 06 eseguito

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -33,16 +33,22 @@ export class TodoList extends React.Component {
     })
   }
 
+  handleRemove(index) {
+    let items = this.state.items;
+    items.splice(index, 1);
+    this.setState({ items });
+  }
+
   render() {
     return (
       <div>
         <p>Buoni propositi del Gran Cavaliere</p>
         <ul>
           {this.state.items.map((item, index) => (
-            <li key={index + 1}>{item}</li>
+            <li key={index + 1}>{item}<button onClick={() => this.handleRemove(index)}>Rimuovi</button></li>
           ))}
-          <br/>
         </ul>
+        <br/>
         <input value={this.state.input} onChange={this.handleInputSave}></input>
         <button onClick={this.state.input !== "" ? this.handleInputAdd : false}>Add to list</button>
         <button onClick={this.handleReset}>Reset</button>
