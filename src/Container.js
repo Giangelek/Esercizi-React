71db46756d8ad520fc0fa5539dbf3ee6f59f78ce
Gianfranco D'Angelo <Gianfranco.dangelo94@gmail.com>
2022-11-26 10:55:50 +0100

    comp comp 2 eseguito

--- src/Container.js
+++ src/Container.js
@@ -2,6 +2,11 @@ import React from "react";
 
 export class Container extends React.Component {
   render() {
-    return <div className="container">{this.props.children}</div>;
+    return (
+      <div className="container">
+        <divm className="container_titolo">{this.props.title}</divm>
+        <div className="container_contenuto">{this.props.children}</div>
+      </div>
+    );
   }
 }
