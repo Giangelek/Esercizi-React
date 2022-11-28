86566e118b445f06f2c9caa47490d81cc7276319
Gianfranco D'Angelo <Gianfranco.dangelo94@gmail.com>
2022-11-28 13:49:53 +0100

    .

--- src/UncontrolledLogin.js
+++ src/UncontrolledLogin.js
@@ -1,63 +1,80 @@
 import React from "react";
 
 export class UncontrolledLogin extends React.Component {
-    state={
-        disabled:true,
-        formValue:{
-            username:"",
-            password:"",
-        },
+  state = {
+    disabled: true,
+    formValue: {
+      username: "",
+      password: "",
+    },
+  };
+
+  handleFormLogin = (event) => {
+    event.preventDefault();
+
+    const username = event.target.elements.username.value;
+    const password = event.target.elements.password.value;
+    const remember = event.target.elements.remember.checked;
+
+    console.log({
+      username,
+      password,
+      remember,
+    });
+  };
+
+  handleFormReset = (event) => {
+    event.target.reset();
+    this.setState({
+      disabled: true,
+      formValue: { username: "", password: "" },
+    });
+  };
+
+  handleChange = (event) => {
+    console.log(this.state.formValue);
+    this.setState({
+      formValue: {
+        ...this.state.formValue,
+        [event.target.name]: event.target.value,
+      },
+    });
+    if (
+      this.state.formValue.username !== "" &&
+      this.state.formValue.password !== ""
+    ) {
+      this.setState({ disabled: false });
+    } else {
+      this.setState({ disabled: true });
     }
+  };
 
-    handleFormLogin = (event) => {
-        event.preventDefault();
-    
-        const username = event.target.elements.username.value;
-        const password = event.target.elements.password.value;
-        const remember = event.target.elements.remember.checked;
-    
-        console.log({
-          username,
-          password,
-          remember,
-        });
-    };
-
-    handleFormReset= (event) =>{
-        event.target.reset();
-        this.setState({disabled:true})
-    };
-
-    handleChange= (event) =>{
-        console.log(this.state.formValue)
-        this.setState({
-          formValue: { ...this.state.formValue, [event.target.name]: event.target.value,
-        },
-        });
-        if(this.state.formValue.username !== "" && this.state.formValue.password !== ""){
-            this.setState({disabled : false})
-        } else {
-            this.setState({disabled : true})
-        }
-    }
-
-    render() {
-        return (
-        <div>
-            <p>Prego inserisca i suoi codici d'accesso:</p>
-            <form onSubmit={this.handleFormLogin} onReset={this.handleFormReset} onChange={this.handleChange}>
-                <label>Username</label>
-                <input name="username" autoFocus/>
-                <br/>
-                <label>Password</label>
-                <input name="password" type="password"/>
-                <br/>
-                <label>Ricorda</label>
-                <input name="remember" type="checkbox"/>
-                <br/>
-                <button type="submit" disabled={this.state.disabled}>Login</button>
-                <button type="reset" disabled={this.state.disabled}>Reset</button>
-            </form>
-        </div>)
-    }
+  render() {
+    return (
+      <div>
+        <p>Prego inserisca i suoi codici d'accesso:</p>
+        <form
+          onSubmit={this.handleFormLogin}
+          onReset={this.handleFormReset}
+          onChange={this.handleChange}
+        >
+          <label>Username</label>
+          <input name="username" />
+          <br />
+          <label>Password</label>
+          <input name="password" type="password" />
+          <br />
+          <label>Ricorda</label>
+          <input name="remember" type="checkbox" />
+          <br />
+          <button type="submit" disabled={this.state.disabled}>
+            Login
+          </button>
+          <button type="reset" disabled={this.state.disabled}>
+            Reset
+          </button>
+        </form>
+      </div>
+    );
+  }
 }
