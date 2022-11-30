35b542c6137318f74e75106363952f60d9a0b0b2
Gianfranco D'Angelo <Gianfranco.dangelo94@gmail.com>
2022-11-30 18:43:53 +0100

    react router 4 eseguito

--- src/welcome.js
+++ src/welcome.js
@@ -1,22 +1,14 @@
 import React from "react";
-import { Link, useNavigate } from "react-router-dom";
+
 import { Age } from "./Age";
 
 export function Welcome({ name, age }){
-    const navigate = useNavigate()
-    function handleDatabaseButtonClick(event){
-        const {value} = event.target
-        navigate(value)
-    }
+    
     return(
         <div className="welcome">
             <h1>Welcome, {name}</h1>
             <Age age={age}/>
-            <div className="linkBar">
-                <Link to="/counter">Vai al contatore clienti</Link>
-                <button onClick={handleDatabaseButtonClick} value="users/giangelek">Vai al database ricercati</button>
-
-            </div>
+            
         </div>
     )
 }
