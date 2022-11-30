824675adc094911c9465c3309947077dc1ac0486
Gianfranco D'Angelo <Gianfranco.dangelo94@gmail.com>
2022-11-30 17:03:10 +0100

    router 2 eseguito

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import {React} from "react";
 import { Routes,Route } from "react-router-dom"
 import { Welcome } from "./Welcome";
 import { Container } from "./Container";
+import {Counter } from "./Counter"
 
 export function App() {
   
@@ -10,6 +11,7 @@ export function App() {
       <Container>
         <Routes>
           <Route path="/" element={<Welcome name="Peppino" age={75} />} />
+          <Route path="counter" element={<Counter initialValue={0}/>} />
         </Routes>
       </Container>
     </div>
