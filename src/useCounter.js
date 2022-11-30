30a7de43f788a929e38ee4eb7f54f6bab19ddd81
Gianfranco D'Angelo <Gianfranco.dangelo94@gmail.com>
2022-11-30 15:51:36 +0100

    .

--- src/useCounter.js
+++ src/useCounter.js
@@ -1,15 +1,29 @@
 import { useState } from "react"
 
-export function useCounter(initialValue = 0){
-    const [counter, setCounter] = useState(initialValue)
-    
-    function handleCounterIncrement() {
-      setCounter((c) => c + 1 ) 
-    }
-  
-    return {
-      counter: counter,
-      handleCounterIncrement: handleCounterIncrement,
-    }
-  
-  }
+import { useState, useCallback } from "react"
+
+export function useCounter(initialValue = 0) {
+  const [counter, setCounter] = useState(initialValue);
+
+  const handleCounterIncrement = useCallback(function handleCounterIncrement() {
+    setCounter((c) => c + 1);
+  }, []);
+
+  const handleCounterDecrement = useCallback(function handleCounterDecrement() {
+    setCounter((c) => c - 1);
+  }, []);
+
+  const handleCounterReset = useCallback(
+    function handleCounterReset() {
+      setCounter(initialValue);
+    },
+    [initialValue]
+  );
+
+  return {
+    counter: counter,
+    onIncrement: handleCounterIncrement,
+    onDecrement: handleCounterDecrement,
+    onReset: handleCounterReset,
+  };
+}
