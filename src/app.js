53160172e3271e88c9cd92c6485c5d3fcb2cfa4e
Gianfranco D'Angelo <Gianfranco.dangelo94@gmail.com>
2022-11-30 18:37:36 +0100

    .

--- src/app.js
+++ src/app.js
@@ -1,20 +1,32 @@
 import {React} from "react";
-import { Routes,Route } from "react-router-dom"
+import { Routes,Route, useNavigate } from "react-router-dom"
 import { Welcome } from "./Welcome";
 import { Container } from "./Container";
 import {Counter } from "./Counter"
 import { ShowGithubUser } from "./ShowGithubUser";
 
 export function App() {
+  const navigate = useNavigate();
+  function handleDatabaseButtonClick(event) {
+    const { value } = event.target;
+    navigate(value);
+  }
   
   return (
     <div>
       <Container>
+      <div className="linkBar">
+          <button onClick={handleDatabaseButtonClick} value="/welcome">Vai alla pagina profilo</button>
+          <button onClick={handleDatabaseButtonClick} value="/counter">Vai al contatore clienti</button>
+          <button onClick={handleDatabaseButtonClick} value="users/giangelek">Vai al database ricercati</button>
+
+      </div>
         <Routes>
-          <Route path="/" element={<Welcome name="Peppino" age={75} />} />
+          <Route path="/welcome" element={<Welcome name="Peppino" age={75} />} />
           <Route path="counter" element={<Counter initialValue={0}/>} />
           <Route path="users/:username" element={<ShowGithubUser  />} />
         </Routes>
+        
       </Container>
     </div>
   );
