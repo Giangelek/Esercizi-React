d38d0c16aff633a6f297e31254464dcb86c5dc64
Gianfranco D'Angelo <Gianfranco.dangelo94@gmail.com>
2022-11-30 13:21:08 +0100

    custom hook 04 eseguito

--- src/GithubUser.js
+++ src/GithubUser.js
@@ -5,9 +5,12 @@ export function GithubUser({ username }) {
 
   return (
     <div className="HookForm">
+      <h2>DRC -- Database ricerca criminali</h2>
       {error && <h1>Non è stato possibile trovare l'utente...</h1>}
       {loading && <h1>Ricerca utente...</h1>}
-      {data && <h1>Il nome dell'utente è {data.name}</h1>}
+      {data && <h1>Il nome del ricercato è {data.name}</h1>}
+      <p className="pUser">Il servizio vi è stato offerto dalla società OCP</p>
+      <img  src="https://seeklogo.com/images/O/ocp-logo-19A26FF17B-seeklogo.com.png"  alt="" width="70" height="70" background-color="white" /> 
     </div>
   );
 }
