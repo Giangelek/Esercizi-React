852ada9befd68eb961b22aa608257bdf22302d77
Gianfranco D'Angelo <Gianfranco.dangelo94@gmail.com>
2022-12-01 17:19:43 +0100

    swr 1 eseguito

--- src/useGithubUser.js
+++ src/useGithubUser.js
@@ -4,10 +4,14 @@ import useSWR from "swr";
 const fetcher = (url) => fetch(url).then((response) => response.json());
 
 export function useGithubUser(username) {
-  const { data, error } = useSWR(
+  const { data, error, mutate} = useSWR(
     `https://api.github.com/users/${username}`,
     fetcher
   );
+
+  function onMutate(){
+    mutate()
+  }
   
-  return { data, error, loading: !data && !error };
+  return { data, error, loading: !data && !error, onMutate };
 }
